16c1429dac7a7a35dde427e0de7590e41f8ba4f9
Josh Young <j22cal@gmail.com>
2019-04-17 09:38:04 -0500

    Add routing and basic pages

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Test</div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
     )
   }
 }
